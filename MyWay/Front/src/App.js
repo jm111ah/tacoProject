3de50bd9f087cbddcf632e68e998e8bcf7e1dfc9
tacoyaggi93 <tacoyaggi93@triptopaz.com>
2023-03-07 17:57:22 +0900

    myway project 수정

---
+++ MyWay/Front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Default from './Component/default'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Default></Default>
+    </div>
+  );
+}
+
+export default App;
---
+++ MyWay/Front/src/Layout/Layout.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import Footer from './Footer';
+import '../css/Layout.css';
+import '../css/styles.css';
+
+export default function Layout(){
+    return (
+        <>
+        <Header></Header>
+        </>
+); 
+}
+
+
